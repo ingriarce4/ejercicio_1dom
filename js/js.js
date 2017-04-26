efa45cf2a3c8107de277026442c3695f05a7cca7
ingriarce4 <ingridarce4@gmail.com>
2017-04-26 14:07:08 -0300

    initial commit

---
+++ js/js.js
@@ -0,0 +1,3 @@
+ var enlaces = document.getElementsByTagName("a");
+ var respuesta = document.getElementById("respuesta");
+ respuesta.innerHTML = "En ésta página hay " + (enlaces.length) + " enlaces."
